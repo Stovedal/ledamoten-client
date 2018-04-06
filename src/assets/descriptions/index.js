a487b385d767352e9440a1ff1a8621913fa11623
SarahHale <id15she@cs.umu.se>
2018-04-06 11:01:51 +0200

    more descriptions

--- src/assets/descriptions/index.js
+++ src/assets/descriptions/index.js
@@ -9,4 +9,11 @@ const descriptions = [
   ,
   "Jag är en mysig person med mycket åsikter som vill träffa min stora kärlek"
   ,
+  "Min dröm är att göra världen till en bättre plats! Tillsammans, du och jag, är det möjligt"
+  ,
+  "Djur, natur, lösa korsord och lyssna på Löfven är min passion"
+  ,
+  "Jag är en aktiv person som älskar träning. Att vara med att påverka Sverige till det bättre är en bra pulshöjare ;)"
+  ,
+  "Vad är väl en bal på slottet om man istället kunde ta ett gott glas vin tillsammans med dig :*"
 ]
